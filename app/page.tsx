ce9f5b72bccd57e3cb1bfef8f625b55ba9be1e63
edwardshturman <60242511+edwardshturman@users.noreply.github.com>
2025-02-04 02:49:38 -0800

    Style landing page and new add to calendar button

--- app/page.tsx
+++ app/page.tsx
@@ -1,31 +1,39 @@
+import Link from "next/link"
+import { Button } from "@/components/Button"
+
 import styles from "./page.module.css"
 
 export default function Home() {
   return (
     <div className={styles.container}>
-      <h1 className={styles.title}>BLOOM</h1>
-      <div className={styles.buttonWrapper}>
+      <div className={styles.info}>
+        <h1 id={styles.title}>BLOOM</h1>
+        <p className={styles.subtitle}>Feb. 28 – Mar. 2</p>
+        <p className={styles.subtitle}>Social Hive</p>
       </div>
-      <div className={styles.socialLinks}>
-        <a
+      <Button
+        destination="https://calendar.google.com/calendar/render?action=TEMPLATE&dates=20250228T180000/20250302T160000&ctz=America/Los_Angeles&text=BLOOM+Hackathon&location=Social+Hive&details=The+hackathon+for+the+arts+%26+sciences,+join+compsigh+from+February+28th+%E2%80%93+March+2nd+for+our+first-ever+Spring+hackathon:+BLOOM.+Grab+a+few+friends+%E2%80%94+or+meet+some+new+ones+at+our+pre-hackathon+social!+%E2%80%94+and+create+that+thing+you%27ve+always+wanted+to.+Web+apps,+research+papers,+open+source+contributions,+documentaries,+music+%E2%80%94+all+kinds+of+projects+from+all+walks+of+creative+life+are+welcome!+Make+something+human+with+us.+Go+to+https://bloom.build.">
+          Add to Google Calendar
+      </Button>
+      <div className={styles.socials}>
+        <Link
           href="https://discord.gg/compsigh"
-          className={styles.socialLink}
-          target="_blank"
-          rel="noopener noreferrer"
+          className={styles.social}
         >
           Discord
-        </a>
-        <a
-          href="https://www.instagram.com/compsigh.club/"
-          className={styles.socialLink}
-          target="_blank"
-          rel="noopener noreferrer"
+        </Link>
+        <Link
+          href="https://instagram.com/compsigh.club"
+          className={styles.social}
         >
           Instagram
-        </a>
-        <a href="https://compsigh.club/" className={styles.socialLink} target="_blank" rel="noopener noreferrer">
+        </Link>
+        <Link
+          href="https://compsigh.club"
+          className={styles.social}
+        >
           Website
-        </a>
+        </Link>
       </div>
     </div>
   )
