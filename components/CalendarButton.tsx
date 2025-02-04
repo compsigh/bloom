aa614d8b96ac082807ee60a58ae799d9334a1c0e
Jet Pham <55770902+jetpham@users.noreply.github.com>
2025-02-04 01:44:41 -0800

    customizing button

--- components/CalendarButton.tsx
+++ components/CalendarButton.tsx
@@ -16,9 +16,49 @@ export default function CalendarButton() {
       availability="busy"
       options="'Apple','Google','iCal'"
       listStyle="overlay"
-      buttonStyle="flat"
+      buttonStyle="date"
       hideBackground
       lightMode="system"
+      styleLight="--date-btn-background: white;
+            --list-hover-text: black;
+            --btn-border: white;
+            --date-btn-hover-background: white;
+            --date-btn-text-secondary: black;
+            --btn-text: black;
+            --date-btn-cal-background: black;
+            --date-btn-cal-month-text: white;
+            --date-btn-cal-day-text: white;
+            --date-btn-text: black;
+            --list-hover-background: white;
+            --list-text: white;
+            --btn-hover-background: black;
+            --list-background: black;
+            --btn-background: white;
+            --btn-text: black;
+            --input-background: white;
+            --list-close-background: white;
+            --font: daydream;
+            --btn-border-radius: 0;"
+      styleDark="--date-btn-background: black;
+           --list-hover-text: white;
+           --btn-border: black;
+           --date-btn-hover-background: black;
+           --date-btn-text-secondary: white;
+           --btn-text: white;
+           --date-btn-cal-background: white;
+           --date-btn-cal-month-text: black;
+           --date-btn-cal-day-text: black;
+           --date-btn-text: white;
+           --list-hover-background: black;
+           --list-text: black;
+           --btn-hover-background: white;
+           --list-background: white;
+           --btn-background: black;
+           --btn-text: white;
+           --input-background: black;
+           --list-close-background: black;
+           --font: daydream;
+           --btn-border-radius: 0;"
     />
   )
 }
