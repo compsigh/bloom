126ab8646c436cbf742ec990560e057415ed79c6
Jet Pham <55770902+jetpham@users.noreply.github.com>
2025-02-04 00:41:29 -0800

    absolute basics

---
+++ components/CalendarButton.tsx
@@ -0,0 +1,25 @@
+"use client"
+import { AddToCalendarButton } from "add-to-calendar-button-react"
+
+export default function CalendarButton() {
+  return (
+    <AddToCalendarButton
+      name="BLOOM Hackathon"
+      description="compsigh's Spring hackathon! All students are welcome to spend the weekend developing an interesting project. It'll be judged by a panel of profesors and professionals in industry."
+      startDate="2025-02-28"
+      startTime="18:00"
+      endDate="2025-03-02"
+      endTime="16:00"
+      timeZone="America/Los_Angeles"
+      location="Social Hive (Harney First Floor)"
+      organizer="Jet Pham|jtpham9@dons.usfca.edu"
+      availability="busy"
+      options="'Apple','Google','iCal'"
+      listStyle="overlay"
+      buttonStyle="flat"
+      hideBackground
+      lightMode="system"
+    ></AddToCalendarButton>
+  )
+}
+
