703e5d8ee31fbc7db65ca7024711020e89d61a99
edwardshturman <60242511+edwardshturman@users.noreply.github.com>
2025-02-11 15:40:41 -0800

    Restructure & restyle header based on Figma layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link"
+import Image from "next/image"
 import { Button } from "@/components/Button"
 
 import styles from "./page.module.css"
@@ -7,27 +8,26 @@ export default function Home() {
   return (
     <main tabIndex={-1}>
       <div className={styles.container}>
-        <div className={styles.info}>
-          <h1 id={styles.title}>BLOOM</h1>
-          <p
-            className={styles.subtitle}
-            style={{
-              paddingBottom: "16px",
-              borderBottom: "2px dashed var(--foreground-20)"
-            }}
-          >
-            The hackathon for <br /> the arts &amp; sciences
+        <header id={styles.header}>
+          <div className={styles.wordmark}>
+            <div className={styles.logo}>
+              <Image
+                src={"/icons/logo.svg"}
+                fill={true}
+                alt="BLOOM logo"
+              />
+            </div>
+            <h1 id={styles.title}>BLOOM</h1>
+          </div>
+          <p className={styles.subtitle}>
+            The hackathon for the arts &amp; sciences
           </p>
-          <p
-            className={styles.subtitle}
-            style={{
-              paddingTop: "16px"
-            }}
-          >
-            Feb. 28 – Mar. 2
+          <p className={styles.subtitle}>
+            Feb. 28th – Mar. 2
+            <br />
+            Social Hive
           </p>
-          <p className={styles.subtitle}>Social Hive</p>
-        </div>
+        </header>
         <Button
           destination="https://calendar.google.com/calendar/render?action=TEMPLATE&dates=20250228T180000/20250302T160000&ctz=America/Los_Angeles&text=BLOOM+Hackathon&location=Social+Hive&details=The+hackathon+for+the+arts+%26+sciences,+join+compsigh+from+February+28th+%E2%80%93+March+2nd+for+our+first-ever+Spring+hackathon:+BLOOM.+Grab+a+few+friends+%E2%80%94+or+meet+some+new+ones+at+our+pre-hackathon+social!+%E2%80%94+and+create+that+thing+you%27ve+always+wanted+to.+Web+apps,+research+papers,+open+source+contributions,+documentaries,+music+%E2%80%94+all+kinds+of+projects+from+all+walks+of+creative+life+are+welcome!+Make+something+human+with+us.+Go+to+https://bloom.build."
         >
