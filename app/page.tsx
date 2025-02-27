a4efdf99d74ea5002b802f98859ba22850b11645
gurshdev <95269077+gurshdev@users.noreply.github.com>
2025-02-27 15:13:10 -0800

    Add button to Agenda page

--- app/page.tsx
+++ app/page.tsx
@@ -28,6 +28,9 @@ export default function Home() {
           <Button destination="https://instagram.com/compsigh.club">
             Instagram
           </Button>
+          <Button destination="/agenda">
+            Agenda
+          </Button>
           <Button
             destination="https://calendar.google.com/calendar/render?action=TEMPLATE&dates=20250228T180000/20250302T160000&ctz=America/Los_Angeles&text=BLOOM+Hackathon&location=Social+Hive&details=The+hackathon+for+the+arts+%26+sciences,+join+compsigh+from+February+28th+%E2%80%93+March+2nd+for+our+first-ever+Spring+hackathon:+BLOOM.+Grab+a+few+friends+%E2%80%94+or+meet+some+new+ones+at+our+pre-hackathon+social!+%E2%80%94+and+create+that+thing+you%27ve+always+wanted+to.+Web+apps,+research+papers,+open+source+contributions,+documentaries,+music+%E2%80%94+all+kinds+of+projects+from+all+walks+of+creative+life+are+welcome!+Make+something+human+with+us.+Go+to+https://bloom.build."
           >
