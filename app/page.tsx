ceddb88720096fcb51f309158fd525834078981f
edwardshturman <60242511+edwardshturman@users.noreply.github.com>
2025-02-26 22:37:39 -0800

    Componentize header

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
-import Link from "next/link"
 import Image from "next/image"
 import { Game } from "@/components/Game"
 import { Label } from "@/components/Label"
+import { Header } from "@/components/Header"
 import { Button } from "@/components/Button"
 import { Mention } from "@/components/Mention"
 
@@ -25,27 +25,7 @@ export default function Home() {
   return (
     <main tabIndex={-1}>
       <div className={styles.container}>
-        <header id={styles.header}>
-          <div className={styles.wordmark}>
-            <div className={styles.logo}>
-              <Image
-                src={"/icons/logo.svg"}
-                fill={true}
-                alt="BLOOM logo"
-              />
-            </div>
-            <h1 id={styles.title}>BLOOM</h1>
-          </div>
-          <p className={styles.subtitle}>
-            A hackathon from <Link href="https://compsigh.club">compsigh</Link>, the social computer science club at USF for meeting cool people &amp; building cool things
-          </p>
-          <p className={styles.subtitle}>
-            Feb. 28 – Mar. 2
-            <br />
-            Social Hive
-          </p>
-        </header>
-
+        <Header />
         <div className={styles.socials}>
           <Button destination="https://instagram.com/compsigh.club">
             Instagram
