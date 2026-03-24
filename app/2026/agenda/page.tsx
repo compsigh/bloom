9c9cfb2fab97936f8e1b39a968d18930149108c4
David Shubov <david.shubov@gmail.com>
2026-03-23 17:28:42 -0700

    update: 2026

---
+++ app/2026/agenda/page.tsx
@@ -0,0 +1,26 @@
+import { Header } from "@/components/Header"
+import { Footer } from "@/components/Footer"
+import { Button } from "@/components/Button"
+import { Cascade } from "@/components/Cascade"
+
+import { WhatsOnTheAgenda } from "@/app/2026/Info"
+
+import styles from "./Agenda.module.css"
+
+export default function Agenda() {
+  return (
+    <main tabIndex={-1}>
+      <div className={styles.container}>
+        <Header />
+        <Button destination="/2026">&lt; Back</Button>
+        <div className={styles.agenda}>
+          <h2 className={styles.title}>
+            <Cascade text="Agenda" duration={0.2} />
+          </h2>
+          <WhatsOnTheAgenda />
+        </div>
+        <Footer />
+      </div>
+    </main>
+  )
+}
