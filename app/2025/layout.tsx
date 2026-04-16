e671b3a8aae41edb8aba4b1289b7e41aa2f35562
edwardshturman <60242511+edwardshturman@users.noreply.github.com>
2026-04-15 23:56:55 -0700

    Fix OG image & desc

--- app/2025/layout.tsx
+++ app/2025/layout.tsx
@@ -1,4 +1,12 @@
+import type { Metadata } from "next"
+
 import "./globals.css"
+
+export const metadata: Metadata = {
+  openGraph: {
+    images: "/og-2025.png"
+  }
+}
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return children
