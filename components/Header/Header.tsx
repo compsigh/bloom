1e6a06c9b268c70ab47e8f2b2fcca35e2f36b3e0
edwardshturman <60242511+edwardshturman@users.noreply.github.com>
2026-04-26 01:42:43 -0700

    Add Submit Project button

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,29 +1,32 @@
 import Link from "next/link"
 import Image from "next/image"
 
+import { LinkButton } from "@/components/LinkButton"
+
 import styles from "./Header.module.css"
 
 export function Header() {
   return (
-    <>
-      <header id={styles.header}>
-        <div id={styles.wordmark}>
-          <div id={styles.logo}>
-            <Image src={"/icons/logo.svg"} fill={true} alt="BLOOM logo" />
-          </div>
-          <h1 id={styles.title}>BLOOM</h1>
+    <header id={styles.header}>
+      <div id={styles.wordmark}>
+        <div id={styles.logo}>
+          <Image src={"/icons/logo.svg"} fill={true} alt="BLOOM logo" />
         </div>
-        <p className={styles.subtitle}>
-          A celebration of passion projects, hosted by{" "}
-          <Link href="https://compsigh.club">compsigh</Link>, the computer
-          science club for meeting cool people &amp;&amp; building cool things
-        </p>
-        <p className={styles.subtitle}>
-          Apr 24 – 26
-          <br />
-          Social Hive
-        </p>
-      </header>
-    </>
+        <h1 id={styles.title}>BLOOM</h1>
+      </div>
+      <p className={styles.subtitle}>
+        A celebration of passion projects, hosted by{" "}
+        <Link href="https://compsigh.club">compsigh</Link>, the computer science
+        club for meeting cool people &amp;&amp; building cool things
+      </p>
+      <p className={styles.subtitle}>
+        Apr 24 – 26
+        <br />
+        Social Hive
+      </p>
+      <LinkButton href="https://compsigh.notion.site/34ea3b32ce17801eb3fce003d5e55f9f">
+        Submit Project
+      </LinkButton>
+    </header>
   )
 }
